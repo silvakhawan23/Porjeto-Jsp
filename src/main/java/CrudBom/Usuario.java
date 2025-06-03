c7204e626a3a6f5c26f9d083d22ad70d0f83f049
silvakhawan23 <140752643+silvakhawan23@users.noreply.github.com>
2025-06-03 09:28:05 -0300

    inclusão do Usuario

--- src/main/java/CrudBom/Usuario.java
+++ src/main/java/CrudBom/Usuario.java
@@ -1,5 +1,45 @@
 package CrudBom;
 
 public class Usuario {
+	private int id ;
+	private String nome ;
+	private String email;
+	private String municipio ;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(String municipio) {
+		this.municipio = municipio;
+	}
+	public Usuario(int id, String nome, String email, String municipio) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.municipio = municipio;
+	}
+	public Usuario() {
+		super();
+	}
+	
+	
 
 }
